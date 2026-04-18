eed101cd21a4e1b7d20a06b2925964b72a3ff448
AbdulRehmanAntall <abdulrehmanantall420@gmail.com>
2026-04-18 20:54:01 +0500

    Fixed a bug

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,10 +10,10 @@ import LandingPage from './pages/LandingPage'
 import LoginPage from './pages/LoginPage'
 // import SignUpPage from './pages/SignUpPage'
 
-// import DashboardPage from './pages/DashboardPage'
+//import DashboardPage from './pages/DashboardPage'
 // import JobFinderPage from './pages/JobFinderPage'
-// import AvailabilityManagerPage from './pages/AvailabilityManagerPage'
-// import CalendarPage from './pages/CalendarPage'
+import AvailabilityManagerPage from './pages/AvailabilityManagerPage'
+import CalendarPage from './pages/CalendarPage'
 // import DailyNewsPage from './pages/DailyNewsPage'
 // import SmartEmailPage from './pages/SmartEmailPage'
 import ClassroomPendingWorkPage from './pages/ClassroomPendingWorkPage'
@@ -48,10 +48,10 @@ function AppRoutes() {
       {// ================= PROTECTED ROUTES =================
         (
 
-          // <Route path="/dashboard" element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} />
+         // <Route path="/dashboard" element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} />
           // <Route path="/job-finder" element={<ProtectedRoute><JobFinderPage /></ProtectedRoute>} />
-          //<Route path="/availability" element={<ProtectedRoute><AvailabilityManagerPage /></ProtectedRoute>} />
-          //<Route path="/calendar" element={<ProtectedRoute><CalendarPage /></ProtectedRoute>} />
+         // <Route path="/availability" element={<ProtectedRoute><AvailabilityManagerPage /></ProtectedRoute>} />
+        //<Route path="/calendar" element={<ProtectedRoute><CalendarPage /></ProtectedRoute>} />
           // <Route path="/news" element={<ProtectedRoute><DailyNewsPage /></ProtectedRoute>} />
           //<Route path="/email" element={<ProtectedRoute><SmartEmailPage /></ProtectedRoute>} />
           <Route path="/classroom" element={<ProtectedRoute><ClassroomPendingWorkPage /></ProtectedRoute>} />
